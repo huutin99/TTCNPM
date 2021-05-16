5ddc73d75dbde9441e1432d7bd163e46dcfd25ab
peko-kuntakku <74392254+peko-kuntakku@users.noreply.github.com>
2021-05-16 20:11:53 +0700

    Delete ttcnpm 202 1811193-react directory

--- ttcnpm 202 1811193-react/src/components/component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import styles from './component.module.css'
-
-const AppComponent = () => {
-  return <div className={styles.container}></div>
-}
-
-export default AppComponent
